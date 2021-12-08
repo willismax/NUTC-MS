7ca6f068a3d6bb549bf6442427996555809894a9
willismax <willismax.com@gmail.com>
2021-12-08 23:06:37 +0800

    Create app.py

---
+++ 1204-LINE-BOT-DEMO/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+
+from googletrans import Translator # Google 翻譯模組
+
+app = Flask(__name__)
+
+from config import * 
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SERET)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+#新增自訂translate_text()函數
+def translate_text(text,dest='en'):
+    """以google翻譯將text翻譯為目標語言
+
+    :param text: 要翻譯的字串，接受UTF-8編碼。
+    :param dest: 要翻譯的目標語言，參閱googletrans.LANGCODES語言列表。
+    """
+    translator = Translator()
+    result = translator.translate(text, dest).text
+    return result
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.source.user_id =='Udeadbeefdeadbeefdeadbeefdeadbeef':
+        return 'OK'
+    if event.message.text[:3] == "@翻英":
+        content = translate_text(event.message.text[3:], "en")
+        message = TextSendMessage(text=content)
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text[:3] == "@翻日":
+        content = translate_text(event.message.text[3:] , "ja")
+        message = TextSendMessage(text=content)
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text[:3] == "@翻中":
+        content = translate_text(event.message.text[3:] , "zh-tw")
+        message = TextSendMessage(text=content)
+        line_bot_api.reply_message(event.reply_token, message)
+    else: line_bot_api.reply_message(event.reply_token,
+                                     TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+    app.run()
